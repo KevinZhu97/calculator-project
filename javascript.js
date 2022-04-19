592aae1c5325a6327ba7b43dd74d3559552d70ac
Kevin Zhu <kevinzhu@Kevins-Air.cpe.cableonda.net>
2022-04-19 10:21:17 -0500

    Clear some calculation bugs and add css to improve the user experience

--- javascript.js
+++ javascript.js
@@ -26,36 +26,28 @@ const lastOperation = document.querySelector('.last-operation');
 let displayValue = 0;
 currentOperation.innerHTML = displayValue
 
-// give number button an eventlistener
+// give number button an eventlistener, delete 0 with substring
 const allNumbers = document.querySelectorAll('.number');
 allNumbers.forEach((number) => {
     number.addEventListener('click', () => {
         displayValue += number.innerHTML;
-        // delete the 0
         currentOperation.innerHTML = `${displayValue.substring(1)}`;
     });
 });
 
-// declare variable to use it later to store first 
-// first value 0, in case user wants to sum righ off the bat? ***
 let firstValue = 0;
 let secondValue = '';
 let symbol = '';
 
-// how do i store all the values?
-// what happens if she presses more than one number and operators, to add before the = sign
 const allOperations = document.querySelectorAll('.operator');
 allOperations.forEach((operator) => {
     operator.addEventListener('click', () => {
         if (firstValue === 0) {
             firstValue = parseInt(currentOperation.innerHTML);
-            console.log(firstValue);
             symbol = operator.id;
-            console.log(symbol);
             displayValue = 0;
         } else if (firstValue > 0) {
             secondValue = parseInt(currentOperation.innerHTML);
-            console.log(symbol);
             currentOperation.innerHTML = operate(window[symbol], firstValue, secondValue);
             symbol = operator.id;
             firstValue = parseInt(currentOperation.innerHTML);
@@ -64,11 +56,7 @@ allOperations.forEach((operator) => {
     });
 });
 
-//for some reason operator does not stay the same for the next block of code, it returns to
-//its original value, got it solved: issue was due to variable shadowing
-
-//uncaught type error, use window[variable] to call function with a variable 
-
+// window[variable] to call function with a variable 
 const equals = document.querySelector('#equals');
 equals.addEventListener('click', function() {
     secondValue = parseInt(displayValue);
@@ -76,16 +64,15 @@ equals.addEventListener('click', function() {
     firstValue = (operate(window[symbol], firstValue, secondValue));
 });
 
-// add clear function
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
     firstValue = 0;
-    secondValue = '';
+    secondValue = 0;
     symbol = '';
+    displayValue = 0;
     currentOperation.innerHTML = 0;
 });
 
-// add delete function, get a hold of the display and edit it
 const erase = document.querySelector('#delete');
 erase.addEventListener('click', () => {
     if (currentOperation.innerHTML.length > 1) {
